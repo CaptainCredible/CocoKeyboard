563f3521d55bfcfe3c011dd0c17a251e24944878
xcorex <xcorex@MANTICORE.local>
2016-07-18 08:32:49 +0700

    rename

--- examples/CocoKeyboard/CocoKeyboard.ino
+++ examples/CocoKeyboard/CocoKeyboard.ino
@@ -1,4 +1,4 @@
-#include "TeenyKeyboard.h"
+#include "CocoKeyboard.h"
 
 char key[] = {'I','Y','O','K','K','K','K','K','O','O','O','O','O'};
 
@@ -8,7 +8,7 @@ void setup() {
   
   
   
-  // don't need to set anything up to use TeenyKeyboard
+  // don't need to set anything up to use CocoKeyboard
 }
 
 
@@ -19,15 +19,15 @@ String randomString(key[note]);
 
   // this is generally not necessary but with some older systems it seems to
   // prevent missing the first character after a delay:
-  TeenyKeyboard.sendKeyStroke(0);
+  CocoKeyboard.sendKeyStroke(0);
   
   // Type out this string letter by letter on the computer (assumes US-style
   // keyboard)
   
-  TeenyKeyboard.print(randomString);
+  CocoKeyboard.print(randomString);
   
-  // It's better to use TeenyKeyboard.delay() over the regular Arduino delay()
+  // It's better to use CocoKeyboard.delay() over the regular Arduino delay()
   // if doing keyboard stuff because it keeps talking to the computer to make
   // sure the computer knows the keyboard is alive and connected
-  TeenyKeyboard.delay(500);
+  CocoKeyboard.delay(500);
 }
--- src/CocoKeyboard.h
+++ src/CocoKeyboard.h
@@ -4,8 +4,8 @@
  * TODO: Make a proper file header. :-)
  * Modified for Digispark by Digistump
  */
-#ifndef __TeenyKeyboard_h__
-#define __TeenyKeyboard_h__
+#ifndef __CocoKeyboard_h__
+#define __CocoKeyboard_h__
 
 #include <Arduino.h>
 #include <avr/pgmspace.h>
@@ -129,9 +129,9 @@ const PROGMEM char usbHidReportDescriptor[USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH] 
 #define KEY_ARROW_LEFT 0x50
 
 
-class TeenyKeyboardDevice : public Print {
+class CocoKeyboardDevice : public Print {
  public:
-  TeenyKeyboardDevice () {
+  CocoKeyboardDevice () {
     cli();
     usbDeviceDisconnect();
     _delay_ms(250);
@@ -207,7 +207,7 @@ class TeenyKeyboardDevice : public Print {
   using Print::write;
 };
 
-TeenyKeyboardDevice TeenyKeyboard = TeenyKeyboardDevice();
+CocoKeyboardDevice CocoKeyboard = TeenyKeyboardDevice();
 
 #ifdef __cplusplus
 extern "C"{
@@ -216,7 +216,7 @@ extern "C"{
 	uchar usbFunctionSetup(uchar data[8]) {
     usbRequest_t    *rq = (usbRequest_t *)((void *)data);
 
-    usbMsgPtr = TeenyKeyboard.reportBuffer; //
+    usbMsgPtr = CocoKeyboard.reportBuffer; //
     if ((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {
       /* class request type */
 
@@ -247,4 +247,4 @@ extern "C"{
 #endif
 
 
-#endif // __TeenyKeyboard_h__
+#endif // __CocoKeyboard_h__
